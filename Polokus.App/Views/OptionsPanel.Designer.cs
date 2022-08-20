182e287bed598987eedb878186073046c7df9254
Bartek <ketrab508@gmail.com>
2022-08-20 16:51:18 +0200

    Options Panel Init

---
+++ Polokus.App/Views/OptionsPanel.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Polokus.App.Views
+{
+    partial class OptionsPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // OptionsPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "OptionsPanel";
+            this.Size = new System.Drawing.Size(245, 553);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Polokus.App/Views/OptionsPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polokus.App.Views
+{
+    public partial class OptionsPanel : UserControl
+    {
+        public OptionsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
