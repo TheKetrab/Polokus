8b520953edff50887b199864a877ce8bd01e764d
Bartek <ketrab508@gmail.com>
2022-12-07 20:32:25 +0100

    Message flow tests

--- Polokus.Tests/NodeHandlersTests/MessageFlowTests.cs
+++ Polokus.Tests/NodeHandlersTests/MessageFlowTests.cs
@@ -1,4 +1,7 @@
-﻿using Polokus.Core.Hooks;
+﻿using Polokus.Core;
+using Polokus.Core.Hooks;
+using Polokus.Core.Interfaces;
+using Polokus.Core.Models;
 using Polokus.Tests.Helpers;
 using System;
 using System.Collections.Generic;
@@ -11,20 +14,89 @@ namespace Polokus.Tests.NodeHandlersTests
     public class MessageFlowTests
     {
         [Test]
-        public async Task MessageBetweenProcesses_Test()
+        public async Task MessageBetweenProcesses_TwoProcessesCommunication_ReachEnd()
         {
             // Arrange
             VisitorHooks visitor = new VisitorHooks(VisitTime.BeforeExecute);
-            var pi = BpmnLoader.LoadBpmnXmlIntoSimpleProcessInstance("msgBetweenProcesses.bpmn");
-            // todo: two processes loading, load as context instance?
+            var cm = BpmnLoader.LoadBpmnXmlIntoContextsManager("msgBetweenProcesses1.bpmn", visitor);
+            var ci = cm.ContextInstances.First().Value;
+            var bpmnProcess = ci.BpmnContext.BpmnProcesses.First(x => x.Id == "Process_1fqg2b6");
+            var startNode = bpmnProcess.GetManualStartNode();
+            var pi = ci.CreateProcessInstance(bpmnProcess);
 
             // Act
-            await pi.RunSimple(visitor);
+            await ci.RunProcessAsync(pi, startNode, null);
 
             // Assert
-            Assert.AreEqual(2, pi.ContextInstance.ScriptProvider.Globals.globals.Count);
-            Assert.AreEqual("start;CustomServiceTask;exclusive;end2", visitor.GetResult());
+            Assert.AreEqual("start;;;;;;;;;;;;;;;end", visitor.GetResult()); // every node visited and end achieved
 
+        }
+
+        [Test]
+        [TestCase("msgBetweenProcesses2.bpmn", new int[] { 0, 1, 2 })]
+        [TestCase("msgBetweenProcesses2.bpmn", new int[] { 0, 2, 1 })]
+        [TestCase("msgBetweenProcesses2.bpmn", new int[] { 1, 0, 2 })]
+        [TestCase("msgBetweenProcesses2.bpmn", new int[] { 1, 2, 0 })]
+        [TestCase("msgBetweenProcesses2.bpmn", new int[] { 2, 0, 1 })]
+        [TestCase("msgBetweenProcesses2.bpmn", new int[] { 2, 1, 0 })]
+        [TestCase("msgBetweenProcesses3.bpmn", new int[] { 0, 1, 2 })]
+        [TestCase("msgBetweenProcesses3.bpmn", new int[] { 0, 2, 1 })]
+        [TestCase("msgBetweenProcesses3.bpmn", new int[] { 1, 0, 2 })]
+        [TestCase("msgBetweenProcesses3.bpmn", new int[] { 1, 2, 0 })]
+        [TestCase("msgBetweenProcesses3.bpmn", new int[] { 2, 0, 1 })]
+        [TestCase("msgBetweenProcesses3.bpmn", new int[] { 2, 1, 0 })]
+        public async Task MessageBetweenProcesses_ThreeManualProcesses1_WaitingForEachOther(string processName, int[] permutation)
+        {
+            // NOTE: processes ids are the same in both files
+
+            // Arrange
+            VisitorHooks visitor = new VisitorHooks(VisitTime.BeforeExecute);
+            var cm = BpmnLoader.LoadBpmnXmlIntoContextsManager(processName, visitor);
+            var ci = cm.ContextInstances.First().Value;
+
+            string[] processIds = new string[3] { "Process_1pq1cix", "Process_0p4rtg3", "Process_1ylxybt" };
+            IBpmnProcess[] bpmnProcesses = processIds.Select(pid => ci.BpmnContext.BpmnProcesses.First(pr => pr.Id == pid)).ToArray();
+            IFlowNode[] startNodes = bpmnProcesses.Select(pr => pr.GetManualStartNode()).ToArray();
+            IProcessInstance[] processInstances = bpmnProcesses.Select(pr => ci.CreateProcessInstance(pr)).ToArray();
+
+            const int timeout = 5;
+
+            // Act
+            for (int i = 0; i<permutation.Length; i++)
+            {
+                ci.StartProcessInstance(
+                    processInstances[permutation[i]],
+                    startNodes[permutation[i]],
+                    timeout);
+
+                await Task.Delay(1000);
+            }
+
+            await Task.Delay(timeout);
+
+            // Assert
+            for (int i = 0; i < permutation.Length; i++)
+            {
+                Assert.AreEqual(ProcessStatus.Finished, processInstances[i].Status);
+            }
+        }
+
+
+        [Test]
+        public async Task MessageBetweenProcesses_SendingData_VariableSent()
+        {
+            // Arrange
+            var cm = BpmnLoader.LoadBpmnXmlIntoContextsManager("msgBetweenProcesses4.bpmn");
+            var ci = cm.ContextInstances.First().Value;
+            var bpmnProcess = ci.BpmnContext.BpmnProcesses.First(x => x.Id == "Process_05l3o9f");
+            var startNode = bpmnProcess.GetManualStartNode();
+            var pi = ci.CreateProcessInstance(bpmnProcess);
+
+            // Act
+            await ci.RunProcessAsync(pi, startNode, null);
+
+            // Assert
+            Assert.AreEqual(12, ci.ScriptProvider.Globals.globals["z"]);
         }
     }
 }
